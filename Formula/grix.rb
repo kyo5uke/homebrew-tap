35d6f3ec0862e58b0b131372131eb9cf2012f203
kyosuke <h.kyosuke0505@gmail.com>
2026-06-26 14:54:27 +0900

    grix v0.2.0

--- Formula/grix.rb
+++ Formula/grix.rb
@@ -1,24 +1,24 @@
 class Grix < Formula
   desc "grep with an index: trigram-indexed code search that answers in milliseconds"
   homepage "https://github.com/kyo5uke/grix"
-  version "0.1.4"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kyo5uke/grix/releases/download/v0.1.4/grix-v0.1.4-aarch64-apple-darwin.tar.gz"
-      sha256 "646851a58e18005ae842ff5248491a1af23ebe1baac00f0fb31987c3c5be740a"
+      url "https://github.com/kyo5uke/grix/releases/download/v0.2.0/grix-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c1560c5476a26db7ef74f374983db0d7e9346001f47eb1cf215133c5d8d146ed"
     end
     on_intel do
-      url "https://github.com/kyo5uke/grix/releases/download/v0.1.4/grix-v0.1.4-x86_64-apple-darwin.tar.gz"
-      sha256 "6f4cdc5855aee95f9687f2e6a8904d057e80f374de9c8a942e46df507d7f0c76"
+      url "https://github.com/kyo5uke/grix/releases/download/v0.2.0/grix-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "c5926203c329d142e5be6e1e8235b0b86987e1d52cec0cd217f9ab7309301d65"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kyo5uke/grix/releases/download/v0.1.4/grix-v0.1.4-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "a39e1987f5f9366cdc5427dce9abed4612e521d309b10b4f2d9bf4e06694b2a0"
+      url "https://github.com/kyo5uke/grix/releases/download/v0.2.0/grix-v0.2.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "a663ffe3d47d001bf902c404477dfcfcb8c86a4c993bea896523aa64ab14429a"
     end
   end
 
